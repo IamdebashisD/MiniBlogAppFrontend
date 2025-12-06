17ca1fd25b10a8c479ed407d36c615069f8cb3ae
IamdebashisD <adebashisdas626@gmail.com>
2025-12-06 18:36:40 +0530

    feat(authSlice): implement authentication slice with login and registration handling

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,71 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { loginUser, registerUser } from './authThunks'
+import { getUser, getAccessToken } from '../../services/token.service'
+
+const initialState = {
+    user: getUser(),
+    token: getAccessToken(),
+    loading: false,
+    error: null,
+    isAuthenticated: !!getAccessToken
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setUserFromStorage: (state, action) => {
+            state.user = action.payload
+            state.isAuthenticated = !!state.user
+        },
+        clearAuthState: (state, action) => {
+            state.user = null;
+            state.token = null;
+            state.isAuthenticated = false;
+            state.error = null;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+
+        //login
+        .addCase(loginUser.pending, (state) => {
+            state.loading = true
+            state.error = null
+        })
+        .addCase(loginUser.fulfilled, (state, action) => {
+            state.loading = false
+            state.user = action.payload.user || action.payload
+            state.token = action.payload.access_token
+            state.isAuthenticated = true 
+        })
+        .addCase(loginUser.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload?.message || action.error?.message
+        })
+
+        // register
+        .addCase(registerUser.pending, (state) => {
+            state.loading = true
+            state.error = null
+        })
+        .addCase(registerUser.fulfilled, (state, action) => {
+            state.loading = false
+        })
+        .addCase(registerUser.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload?.message || action.error?.message
+        })
+        
+        // Logout
+        .addCase(registerUser.fulfilled, (state) => {
+            state.user = null
+            state.token = null
+            state.isAuthenticated = false
+        })
+    }
+
+})
+
+export const { setUserFromStorage, clearAuthState } = authSlice.actions
+export default authSlice.reducer
