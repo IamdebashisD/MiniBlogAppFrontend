fc625235669c0f3790144dbe7dbee9c143b5d0ed
IamdebashisD <adebashisdas626@gmail.com>
2025-12-08 22:19:47 +0530

    feat(store): Add user slice to Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 // Redux store config
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice.js'
+import userReducer from '../features/user/userSlice.js'
 
 const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        user: userReducer
     }
 })
 
