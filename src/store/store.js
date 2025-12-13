6e9da4dc199b13b5936e01c224e023c1ead8769b
IamdebashisD <adebashisdas626@gmail.com>
2025-12-13 23:55:58 +0530

    feat(store): add postReducer to the Redux store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice.js'
 import userReducer from '../features/user/userSlice.js'
-
+import postReducer from '../features/posts/postSlice.js'
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        user: userReducer
+        user: userReducer,
+        post: postReducer
     }
 })
 
