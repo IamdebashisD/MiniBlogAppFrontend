8809ba92ad14a894fee1ab63a8d7a61cfce46b3e
IamdebashisD <adebashisdas626@gmail.com>
2025-12-13 23:57:05 +0530

    feat(App): integrate Redux for fetching and displaying posts with loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Register from './pages/Register'
+import Post from './pages/Post'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchAllPosts } from './features/posts/postThunks.js'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch()
+  const posts = useSelector(state => state.post?.posts || [])
+  const { loading } = useSelector(state => state.post)
+  
+  useEffect(() => {
+    dispatch(fetchAllPosts({page:1, per_page:10}))
+  }, [dispatch])
+
+  if (loading){
+    return <div>Loading...</div>
+  }
+  if(!posts || posts.length === 0){
+    return <div style={{textAlign:'center'}}>No posts yet...</div>
+  } 
+    
 
   return (
     <>
-     <h1 className='bg-[#323232] text-[#F9F4DA] p-6 items-center'>Mini Blog Application</h1>
+     {/* <Register /> */}
+
+     {posts.map(post => (
+      <Post key={post.id} post={post} />
+     ))}
+
     </>
   )
 }
