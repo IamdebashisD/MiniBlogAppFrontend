492020c0274351773239a5db2c4bc70641aaf1b1
IamdebashisD <adebashisdas626@gmail.com>
2025-12-21 01:09:49 +0530

    feat(store): add likeReducer to the Redux store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice.js'
 import userReducer from '../features/user/userSlice.js'
 import postReducer from '../features/posts/postSlice.js'
+import likeReducers from '../features/likes/likeSlice.js'
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         user: userReducer,
-        post: postReducer
+        post: postReducer,
+        like: likeReducers
     }
 })
 
