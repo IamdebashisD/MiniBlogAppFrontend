e2f7228550269401760aeccd126873a0de0467a0
IamdebashisD <adebashisdas626@gmail.com>
2025-12-21 01:15:31 +0530

    update the import name for like reducer from "likeReducers" to "likeReducer"

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice.js'
 import userReducer from '../features/user/userSlice.js'
 import postReducer from '../features/posts/postSlice.js'
-import likeReducers from '../features/likes/likeSlice.js'
+import likeReducer from '../features/likes/likeSlice.js'
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         user: userReducer,
         post: postReducer,
-        like: likeReducers
+        like: likeReducer
     }
 })
 
