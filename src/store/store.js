4bb19e2df861441ed92e1a9729b6c861d1fa2d43
IamdebashisD <adebashisdas626@gmail.com>
2025-12-27 03:34:56 +0530

    feat(store): add comments reducer to the Redux store configuration

--- src/store/store.js
+++ src/store/store.js
@@ -4,13 +4,14 @@ import authReducer from '../features/auth/authSlice.js'
 import userReducer from '../features/user/userSlice.js'
 import postReducer from '../features/posts/postSlice.js'
 import likeReducer from '../features/likes/likeSlice.js'
+import commentReducer from '../features/comments/commentSlice.js'
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         user: userReducer,
         post: postReducer,
-        like: likeReducer
+        like: likeReducer,
+        comments: commentReducer
     }
 })
-
 
